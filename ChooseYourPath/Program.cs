219f760005c2fd800b7a17cf8b8264a7dcc587da
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-09-15 12:35:51 -0500

    First commit

---
+++ ChooseYourPath/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Welcome to ChooseYourPath");
+Console.WriteLine("Type anything to login");
+Console.ReadLine();
+Console.Clear();
+
+while (true)
+{
+    Console.WriteLine("Please enter your username (myusername) ");
+    string userName = Console.ReadLine();
+
+    Console.WriteLine("Please enter your password (mypassword): ");
+    string password = Console.ReadLine();
+
+    if (userName == "myusername" && password == "mypassword")
+    {
+        Console.WriteLine("You are logged in... (Type anything to continue)");
+        Console.ReadLine();
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Wrong password or username... please try again!");
+    }
+}
+
+Console.WriteLine("Time to get serious... (Press 'enter' to start the game)");
+Console.ReadLine();
+
+Console.WriteLine("done");
+
